fd26a3603d5ef198d7c4e95d2409d8535052cf4c
Sirui Chen <632882184@qq.com>
2018-09-13 10:52:03 +0800

    delete unused files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FxLayout from "fx-layout-vue";
 
 Vue.config.productionTip = false
+
+Vue.use(FxLayout);
 
 new Vue({
   render: h => h(App)
